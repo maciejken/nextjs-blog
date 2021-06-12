97de7c7307010f6c19ac9928e5a40876762c0a4e
Maciej Kędra <maciejken@gmail.com>
2021-06-12 19:17:27 +0200

    update about page

--- pages/o-mnie.js
+++ pages/o-mnie.js
@@ -11,8 +11,13 @@ export default function About() {
           W wolnym czasie biegam, jeżdżę na longboardzie, czytam książki lub słucham podcastów.
         </p>
         <p>
-          Ta strona istnieje dzięki malinom zasilanym energią elektryczną.{' '}
-          Zbudujesz podobną krok po kroku z{' '}
+          Ta strona jest skutkiem ubocznym poszukiwań technologii, która pozwalałaby{' '}
+          wygodnie tworzyć lekkie i szybkie strony internetowe. Początkowo chciałem{' '}
+          przechowywać bloga w bazie danych, tak jak robi to WordPress, ale ostatecznie{' '}
+          spodobał mi się Next.js i pisanie do plików tekstowych w formacie markdown.{' '}
+        </p>
+        <p>
+          Jeśli podoba Ci się moja strona, możesz zbudować podobną krok po kroku z{' '}
           <a href="https://nextjs.org/learn" target="_blank">samouczkiem Next.js</a>.
         </p>
       </section>
